9cadff4eb6d9f4021b7f4377306af88f1b57fe17
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-09-28 16:44:45 +0200

    Removed '-' separator for words (back to ' ')

--- front/static/main.js
+++ front/static/main.js
@@ -9,19 +9,22 @@ $('#search-btn').click(function(e) {
   if (!text) return;
 
   $.getJSON(QUERY_URL + formatForUrl(text), function(data) {
-    console.log(data);
-    var html = new EJS({url: '/static/results.ejs'}).render(data);
+    //console.log(data);
+    var html = new EJS({ url: '/static/results.ejs' }).render(data);
     $resultBox.html(html);
   });
 });
 
 function formatForUrl(str) {
-  return str.replace(/_/g, '-')
-    .replace(/ /g, '-')
-    .replace(/:/g, '-')
-    .replace(/\\/g, '-')
-    .replace(/\//g, '-')
-    .replace(/[^a-zA-Z0-9\-]+/g, '')
-    .replace(/-{2,}/g, '-')
-    .toLowerCase();
+  var separator = ' ';
+  return encodeURI(str.replace(/_/g, separator)
+    .replace(/ /g, separator)
+    .replace(/:/g, separator)
+    .replace(/\\/g, separator)
+    .replace(/\//g, separator)
+    //.replace(/[^a-zA-Z0-9\-]+/g, '')
+    .replace(/-{2,}/g, separator)
+    .toLowerCase());
 };
+
+// vim: ft=javascript et sw=2 sts=2
