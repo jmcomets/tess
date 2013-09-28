d36efc48ec6b385a8c2fc45e27bca603b4e9833e
Owen Wang <wangfowen@gmail.com>
2013-09-28 22:01:09 +0200

    script to generate counts based on class names

--- data/scripts/generate_ml_input.rb
+++ data/scripts/generate_ml_input.rb
@@ -1,18 +1,35 @@
+class String
+  def underscore
+    self.gsub(/::/, '/').
+    gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
+    gsub(/([a-z\d])([A-Z])/,'\1_\2').
+    tr("-", "_").
+    downcase
+  end
+end
+
 def generate_ml_input(folder_name)
-  class_map = {}
-
-  Dir.open folder_name do |dir|
-    dir.each do |file|
-      text = file.read
-      classes = text.scan(/class="([\w\-\s]+)"/).map { |a| a[0].split(" ") }.flatten(1)
-
-      classes.each do |c|
-        class_map[c] = class_map[c].nil? ? 1 : class_map[c] + 1
-      end
+  Dir.glob("#{folder_name}/*") do |file|
+    next if file == '.' or file == '..'
+    if File.directory?(file)
+      generate_ml_input(file)
+    else
+      count_classes_in_file(file)
     end
   end
+end
 
-  puts class_map
+def count_classes_in_file(file)
+    class_map = {}
+    text = IO.read(file).force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
+    classes = text.scan(/class="([\w\-\s]+)"/).map { |a| a[0].underscore.gsub("_", " ").gsub("-", " ").split(" ") }.flatten(1)
+
+    classes.each do |c|
+      class_map[c] = class_map[c].nil? ? 1 : class_map[c] + 1
+    end
+
+    puts "file name: #{file}"
+    puts "classes: #{class_map.sort_by {|k,v| v }}"
 end
 
 if !(ARGV.first)
