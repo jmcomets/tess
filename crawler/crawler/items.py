4c3d02b4cdcaea192b942530edb8133b2851cf0e
halflings <ahmed.kachkach@gmail.com>
2013-09-29 00:37:50 +0200

    Fixed bug in the push_wrapper script

--- crawler/crawler/items.py
+++ crawler/crawler/items.py
@@ -8,9 +8,9 @@
 from scrapy.item import Item, Field
 import urlparse
 
-PRICE_MAP = {['€', 'EUR', 'EURO'] : 'EUR',
-             ['£', 'GBP', 'POUND'] : 'GBP',
-             ['$', 'USD', 'DOLLAR']: 'USD'}
+PRICE_MAP = {('€', 'EUR', 'EURO') : 'EUR',
+             ('£', 'GBP', 'POUND') : 'GBP',
+             ('$', 'USD', 'DOLLAR') : 'USD'}
 
 class ProductItem(Item):
     _id = Field()
@@ -30,21 +30,21 @@ class ProductItem(Item):
             if thumbnail.startswith('/'):
                 self['thumbnail'][i] = 'http://' + domain + self['thumbnail'][i]
 
-        # TODO: Parsing prices
-        raw_price = self['price'].strip()
-        self['price'] = None
+        # # TODO: Parsing prices
+        # raw_price = self['price'][0].strip().decode('utf-8')
+        # self['price'] = None
 
-        for signatures, currency in PRICE_MAP.iteritems():
-            for signature in signatures:
-                if signature in raw_price.startswith(signature) or raw_price.endswith('raw_price'):
-                    clean_price = raw_price.replace(signature, '')
-                    self['price'] = {'raw': raw_price,
-                                     'value': float(clean_price),
-                                     'currency': currency}
-                    break
+        # for signatures, currency in PRICE_MAP.iteritems():
+        #     for signature in signatures:
+        #         if signature in raw_price.startswith(signature) or raw_price.endswith('raw_price'):
+        #             clean_price = raw_price.replace(signature, '')
+        #             self['price'] = {'raw': raw_price,
+        #                              'value': float(clean_price),
+        #                              'currency': currency}
+        #             break
             
-            if self['price']:
-                break
+        #     if self['price']:
+        #         break
 
-        if self['price'] is None:
-            self['price'] = {'raw': raw_price, 'value' : None, 'currency': None}
+        # if self['price'] is None:
+        #     self['price'] = {'raw': raw_price, 'value' : None, 'currency': None}
