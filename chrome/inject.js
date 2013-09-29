928aaeef7724eadb27cc8c6e5e5855d8cf636f1a
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-09-29 10:00:31 +0200

    Setting up chrome extension

--- chrome/inject.js
+++ chrome/inject.js
@@ -2,14 +2,14 @@ var TessClient = {
   baseURL: 'http://192.168.66.33:5000',
   label: function(url, yes_no) {
     var xhr = new XMLHttpRequest();
-    xhr.open('POST', baseURL + '/api/label', true);
+    xhr.open('POST', this.baseURL + '/api/label', true);
     xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
     xhr.send('label=' + yes_no + '&url=' + encodeURI(url));
   }
 };
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.type === "label") {
+  if (request.type == 'label') {
     TessClient.label(window.location.href, request.yes_no);
   }
 });
--- chrome/popup.js
+++ chrome/popup.js
@@ -1,26 +1,25 @@
 var TessLabeler = {
   addEventListeners: function() {
     var self = this;
-    document.getElementById('yes').addEventListener('click', function(e) {
-      self.label(true);
-    });
-    document.getElementById('no').addEventListener('click', function(e) {
-      self.label(false);
-    });
+    document.getElementById('yes').addEventListener('click', function(e) { self.label(true); });
+    document.getElementById('no').addEventListener('click', function(e) { self.label(false); });
   }, label: function(yes_no) {
-    var requestContext = {
-      type: "label",
-      yes_no: yes_no
-    };
-
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, requestContext, function(){});
+      chrome.tabs.sendMessage(tabs[0].id, {
+        type: 'label', yes_no: yes_no
+      }, function() {});
+      document.getElementById('label').style.display = 'none';
+      document.getElementById('redirect').style.display = 'block';
+      var aTags = document.getElementsByTagName('a'),
+        url = aTags[Math.floor(Math.random()*aTags.length)];
     });
   }
 };
 
 document.addEventListener('DOMContentLoaded', function () {
   TessLabeler.addEventListeners();
+  document.getElementById('label').style.display = 'block';
+  document.getElementById('redirect').style.display = 'none';
 });
 
 // vim: ft=javascript et sw=2 sts=2
