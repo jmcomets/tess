b7012328f115ee8f219855b5a64a6c42cd22d41b
Owen Wang <wangfowen@gmail.com>
2013-09-29 08:32:14 +0200

    needed more permissions

--- chrome/popup.js
+++ chrome/popup.js
@@ -1,29 +1,11 @@
-/*
-//click handler for the buttons, will prompt you to click on an element to add label
-var addLabel = function(type) {
-  console.log(type);
+var TessLabeler = {
+  addEventListeners: function() {
+    document.getElementById('yes').addEventListener('click', function(e) {
+      alert("here");
+    });
+  }
 };
 
-var element = document.getElementById('price');
-element.addEventListener('click', function() { addLabel('price') }, false);
-
-element = document.getElementById('name');
-element.addEventListener('click', function() { addLabel('name') }, false);
-
-element = document.getElementById('description');
-element.addEventListener('click', function() { addLabel('description') }, false);
-*/
-
-
-//click handler to save page / add to elastic search
-
-window.onload = function() {
-  function clickHandler(e) {
-    window.location = "http://google.com";
-  }
-
-  document.addEventListener('DOMContentLoaded', function () {
-    document.querySelector('button').addEventListener('click', clickHandler);
-  });
-}
-
+document.addEventListener('DOMContentLoaded', function () {
+  TessLabeler.addEventListeners();
+});
