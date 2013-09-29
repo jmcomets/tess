394d26d9bab5491e731548d50f25a7e10b9333df
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-09-29 10:58:31 +0200

    Changed url to vps

--- chrome/inject.js
+++ chrome/inject.js
@@ -1,5 +1,5 @@
 var TessClient = {
-  baseURL: 'http://localhost:5000',
+  baseURL: 'http://92.39.246.129:5000',
   label: function(url, yes_no) {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', this.baseURL + '/api/label', true);
