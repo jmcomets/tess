69e95311b87255c9563f843d4b1a3d700c8a3c13
Jean-Marie Comets <jean.marie.comets@gmail.com>
2013-09-29 04:22:36 +0200

    Better API for prediction

--- back/learning/predict.py
+++ back/learning/predict.py
@@ -1,17 +1,19 @@
 import csv
 import bisect
+import pickle
+import itertools as it
 from sklearn import linear_model
 
-__all__ = ('Predictor')
+__all__ = ('Classifier', 'Predictor')
 
-class Predictor(object):
+class Classifier(object):
     def __init__(self):
-        super(Predictor, self).__init__()
+        super(Classifier, self).__init__()
         self.learned = False
 
-    def learn(self, headers_file, data_file):
-        self.attributes = get_headers(headers_file)
-        data = get_data(self.attributes, data_file)
+    def learn(self, attributes, data):
+        self.attributes = attributes
+        data = format_data(attributes, data)
         self.matrix = data['matrix']
         self.labels = data['labels']
         self.pages = data['pages']
@@ -20,36 +22,49 @@ class Predictor(object):
         self.clf = clf
         self.learned = True
 
+class Predictor(object):
+    YES_NO_BOUNDARY = 0.5
+
+    def __init__(self, classifier):
+        assert classifier.learned, 'Predictor must learn first!'
+        self.cls = classifier
+
     def predict(self, attr_scores):
-        assert self.learned, 'Predictor must learn first!'
-        return 0.5 < self.clf.predict(attr_scores)
+        return [self.YES_NO_BOUNDARY < x for x in self.cls.predict(attr_scores)]
 
-def get_headers(filename):
-    delimiter = ','
-    with open(filename, 'r') as file_:
-        return filter(None, file_.read().strip('\r\n').split(delimiter))
+    @classmethod
+    def from_file(self, fp):
+        cls = Classifier()
+        cls.clf = pickle.load(fp)
+        cls.learned = True
+        return Predictor(cls)
 
-def get_data(attributes, filename):
-    assert sorted(attributes)
+    def dump(self, fp):
+        pickle.dump(self.cls.clf, fp)
+
+def get_headers(fp):
+    delimiter = '\n'
+    return it.ifilter(None, fp.read().strip('\r\n').split(delimiter))
+
+def get_data(attributes, fp):
     # read data from file
-    data = []
-    with open(filename, 'r') as file_:
-        reader = csv.reader(file_, delimiter=',')
-        for row in reader:
-            page_data = {
-                    'page': row.pop(0),
-                    'product': bool(int(row.pop(0))),
-                    'attributes': [],
-                    }
-            for attr_score in row:
-                attr, score = attr_score.split(':')
-                assert attr in attributes
-                page_data['attributes'].append({
-                    'name': attr,
-                    'score': int(score),
-                    })
-            data.append(page_data)
+    reader = csv.reader(fp, delimiter=',')
+    for row in reader:
+        page_data = {
+                'page': row.pop(0),
+                'product': int(row.pop(0)),
+                'attributes': [],
+                }
+        for attr_score in row:
+            attr, score = attr_score.split(':')
+            assert attr in attributes
+            page_data['attributes'].append({
+                'name': attr,
+                'score': int(score),
+                })
+        yield page_data
 
+def format_data(attributes, data):
     # format data to matrix/labels/etc
     pages = []
     labels = []
@@ -69,21 +84,38 @@ def get_data(attributes, filename):
             }
 
 if __name__ == '__main__':
-    import itertools
-    predictor = Predictor()
-    predictor.learn('headers.txt', 'data.csv')
-    test_data = get_data(predictor.attributes, 'data-test.csv')
-    test_cases = test_data['matrix']
-    labels = test_data['labels']
-    pages = test_data['pages']
-    predictions = [x for x in predictor.predict(test_cases)]
-    # compute recall/precision
-    count = lambda ls: sum(itertools.imap(float, ls))
-    intersect = lambda a, b: itertools.imap(lambda x: x[0] and x[1], itertools.izip(a, b))
-    intersect
-    labeled_yes = predictions
-    real_yes = labels
-    recall = count(intersect(labeled_yes, real_yes)) / count(real_yes)
-    precision = count(intersect(labeled_yes, real_yes)) / count(labeled_yes)
-    print 'recall =', recall
-    print 'precision =', precision
+    print 'Loading headers...'
+    with open('headers.txt', 'r') as fp:
+        attributes = list(get_headers(fp))
+        assert sorted(attributes)
+    print 'Loading data...'
+    with open('data.csv', 'r') as fp:
+        data = list(get_data(attributes, fp))
+
+    # five fold cross validation
+    import random
+    for _ in xrange(5):
+        test_size = len(data) / 5
+        random.shuffle(data)
+
+        print 'Classifying data...'
+        cls = Classifier()
+        cls.learn(attributes, data)
+        predictor = Predictor(cls)
+        test_data = format_data(attributes, data[:test_size])
+        test_cases = test_data['matrix']
+        labels = test_data['labels']
+        pages = test_data['pages']
+        predictions = predictor.predict(test_cases)
+
+        # compute recall/precision
+        count = lambda ls: sum(it.imap(float, ls))
+        intersect = lambda a, b: it.imap(lambda x: x[0] and x[1], it.izip(a, b))
+        labeled_yes = predictions
+        real_yes = labels
+        yes_ok_intersect = intersect(labeled_yes, real_yes)
+        yes_ok = count(yes_ok_intersect)
+        recall = yes_ok / count(real_yes)
+        print 'recall =', yes_ok, '/', count(real_yes), '=', recall
+        precision = yes_ok / count(labeled_yes)
+        print 'precision =', yes_ok, '/', count(labeled_yes), '=', precision
