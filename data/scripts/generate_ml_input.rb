06cd9027d9b843f42207b9abe58f3a60d883a437
Owen Wang <wangfowen@gmail.com>
2013-10-15 15:17:31 +0200

    made ruby script slightly more modular

--- data/scripts/generate_ml_input.rb
+++ data/scripts/generate_ml_input.rb
@@ -1,7 +1,11 @@
+#run this by doing "ruby generate_ml_input.rb <FOLDER_NAME>"
+
 require 'set'
 require 'csv'
 
-IS_PRODUCT = ARGV[1] || 1
+#0 = other, 1 = product, 2 = profile
+CLASSIFIED_TYPE = ARGV[1] || 1
+
 DATA_FILE = "data.csv"
 DATA_TEST_FILE = "data-test.csv"
 HEADERS_FILE = "headers.txt"
@@ -12,6 +16,7 @@ CLASS_SET = Set.new
 `touch #{HEADERS_FILE}`
 
 class String
+  #converts camel case to underscore case
   def underscore
     self.gsub(/::/, '/').
     gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
@@ -21,6 +26,7 @@ class String
   end
 end
 
+#from a word get all the subwords
 def get_subwords(word)
   word.underscore.
   gsub("_", " ").
@@ -28,36 +34,21 @@ def get_subwords(word)
   split(" ")
 end
 
-def generate_ml_inputs(folder_name, is_root)
-  if File.directory?(folder_name)
-    Dir.glob("#{folder_name}/*") do |file|
-      next if file == '.' or file == '..'
-      if File.directory?(file)
-        generate_ml_inputs(file, false)
-      else
-        append_data_to_file(file)
-      end
-    end
-  else
-    append_data_to_file(folder_name)
+#randomly return the file you should write to
+def randomized_write_file
+  do_split = rand(5)
+  write_file = DATA_FILE
+
+  #20% chance of writing to the other file
+  if (do_split == 0)
+    write_file = DATA_TEST_FILE
   end
 
-  if is_root
-    to_write = CLASS_SET.to_a.join("\n")
-
-    if (File.size(HEADERS_FILE) != 0)
-        to_write = "\n" + to_write
-    end
-
-    File.open(HEADERS_FILE, "a") { |file| file.write(to_write) }
-
-    `mv #{HEADERS_FILE} temp.txt`
-    `sort temp.txt | uniq > #{HEADERS_FILE}`
-    `rm temp.txt`
-  end
+  write_file
 end
 
-def append_data_to_file(file)
+#count of number of classes in a page - one type of data to learn on
+def create_class_count_map(file)
   class_map = {}
   text = IO.read(file).force_encoding("ISO-8859-1").encode("utf-8", replace: nil)
   classes = text.scan(/[class|id]="([\w\-\s]+)"/).map { |a| get_subwords(a[0]) }.flatten(1)
@@ -67,16 +58,52 @@ def append_data_to_file(file)
     CLASS_SET.add(c)
   end
 
-  do_split = rand(5)
-  write_file = DATA_FILE
+  class_map
+end
 
-  #20% chance of writing to the other file
-  if (do_split == 0)
-    write_file = DATA_TEST_FILE
+#given a file and map of data, puts the data into the file along with the classified type
+def append_data_to_file(file, data_map)
+
+  CSV.open(randomized_write_file(), "ab") do |csv|
+    csv << [file.gsub(",", ""), CLASSIFIED_TYPE, data_map.map { |k,v| "#{k}:#{v}" }].flatten(1)
+  end
+end
+
+#give it a folder and it appends all the data gathered from it into the data file
+def generate_ml_inputs(folder_name, is_root=true)
+  if File.directory?(folder_name)
+    Dir.glob("#{folder_name}/*") do |file|
+      next if file == '.' or file == '..'
+      if File.directory?(file)
+        generate_ml_inputs(file, false)
+      else
+        #change this if use different data to learn on
+        class_map = create_class_count_map(file)
+        append_data_to_file(file, class_map)
+      end
+    end
+  else
+    #change this if use different data to learn on
+    class_map = create_class_count_map(folder_name)
+    append_data_to_file(folder_name, class_map)
   end
 
-  CSV.open(write_file, "ab") do |csv|
-    csv << [file.gsub(",", ""), IS_PRODUCT, class_map.map { |k,v| "#{k}:#{v}" }].flatten(1)
+  #TODO: this should also be abstracted since it only applies when learning by class count
+  if is_root
+    #when all the data on the class set is gather, write it to the headers file
+    to_write = CLASS_SET.to_a.join("\n")
+
+    #if there's already data, append after the existing data
+    if (File.size(HEADERS_FILE) != 0)
+        to_write = "\n" + to_write
+    end
+
+    File.open(HEADERS_FILE, "a") { |file| file.write(to_write) }
+
+    #hacky way of making sure data in HEADERS_FILE is unique and sorted
+    `mv #{HEADERS_FILE} temp.txt`
+    `sort temp.txt | uniq > #{HEADERS_FILE}`
+    `rm temp.txt`
   end
 end
 
@@ -85,4 +112,4 @@ if !(ARGV.first)
   exit
 end
 
-generate_ml_inputs(ARGV.first, true)
+generate_ml_inputs(ARGV.first)
